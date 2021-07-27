7a60af8444894b67c2cbfe0fcecca58045dfddee
유하람 <w2036@e-mirim.hs.kr>
2021-07-27 11:51:21 +0900

    Edit GalleryAdapter.java(Add Toast) Commit

--- app/src/main/java/kr/hs/emirim/w2036/project11_2/GalleryAdapter.java
+++ app/src/main/java/kr/hs/emirim/w2036/project11_2/GalleryAdapter.java
@@ -7,14 +7,21 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Gallery;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GalleryAdapter extends BaseAdapter {
     Context context;
+    ImageView imgvLarge;
+
     int[] posterIds = {R.drawable.i01, R.drawable.i02, R.drawable.i03, R.drawable.i04, R.drawable.i05,
                        R.drawable.i06, R.drawable.i07, R.drawable.i08, R.drawable.i09, R.drawable.i10};
+    int[] titleIds = {R.drawable.i01, R.drawable.i02, R.drawable.i03, R.drawable.i04, R.drawable.i05,
+                      R.drawable.i06, R.drawable.i07, R.drawable.i08, R.drawable.i09, R.drawable.i10};
 
-    public GalleryAdapter(Context context){
+    public GalleryAdapter(Context context, ImageView imgvLarge){
         this.context = context;
+        this.imgvLarge = imgvLarge;
     }
 
     @Override
@@ -35,9 +42,23 @@ public class GalleryAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imgv = new ImageView(context);
-        imgv.setLayoutParams(new ViewGroup.LayoutParams(200, 300));
+        imgv.setLayoutParams(new Gallery.LayoutParams(200, 300));
         imgv.setImageResource(posterIds[position]);
         imgv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+        final int pos = position;
+        imgv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgvLarge.setImageResource(posterIds[pos]);
+                Toast toast = new Toast(context);
+                View toastView = View.inflate(context, R.layout.toast1, null);
+                TextView textTitle = toastView.findViewById(R.id.text_title);
+                textTitle.setText(titleIds[pos]);
+                toast.setView(toastView);
+                toast.show();
+            }
+        });
         return imgv;
     }
 }
