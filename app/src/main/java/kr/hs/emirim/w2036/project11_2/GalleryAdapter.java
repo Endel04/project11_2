a83b8b2b2902342b78d1996aa318d15f450d2645
유하람 <w2036@e-mirim.hs.kr>
2021-07-27 11:11:00 +0900

    Edit GalleryAdapter.java Commit

---
+++ app/src/main/java/kr/hs/emirim/w2036/project11_2/GalleryAdapter.java
@@ -0,0 +1,43 @@
+package kr.hs.emirim.w2036.project11_2;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+public class GalleryAdapter extends BaseAdapter {
+    Context context;
+    int[] posterIds = {R.drawable.i01, R.drawable.i02, R.drawable.i03, R.drawable.i04, R.drawable.i05,
+                       R.drawable.i06, R.drawable.i07, R.drawable.i08, R.drawable.i09, R.drawable.i10};
+
+    public GalleryAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return posterIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imgv = new ImageView(context);
+        imgv.setLayoutParams(new ViewGroup.LayoutParams(200, 300));
+        imgv.setImageResource(posterIds[position]);
+        imgv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        return imgv;
+    }
+}
